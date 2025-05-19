8de0d341840741d1e69f19a9a528bb8cdaa81533
Goberment <mdiego@gmail.com>
2025-05-18 20:37:01 -0700

    Se hicieron pruebas en la terminal

---
+++ main.py
@@ -0,0 +1,67 @@
+from entities.cliente import Cliente
+from entities.producto import Producto
+from persistence.db import SessionLocal
+
+session = SessionLocal()
+
+def get():
+     producto  = session.query(Producto).all()
+     for p in producto:
+          print(f"{p.id}, Nombre: {p.nombre}, Precio: ${p.precio}")
+
+print("-----------Menú-------------")
+dm= input("1.-Crear cuenta" \
+"2.- Iniciar sesión" \
+"3.-Mostrar Catalogo" \
+"4.- Ver opciones de pago" \
+"5.- ver carrito" \
+"6.- ir a pagar")
+
+while(dm=="3" or dm=="4" or dm=="5" or dm=="6"):
+    if dm =="3":
+         
+         get()
+         compra=input("Selecciona un producto")
+         
+         while(compra=="1", compra=="2", compra=="3"):
+            if compra=="1":
+                print("Se agrego al carrito")
+                broken=input("Quieres seleccionar algo mas 1(Si)-2(No)")
+                if broken=="1":
+                    break
+               
+                else:
+                    print("regresar al menu")
+                    break
+                    
+            if compra=="2":
+                print("Se agrego al carrito")
+                broken=input("Quieres seleccionar algo mas 1(Si)-2(No)")
+                if broken=="1":
+                    break
+               
+                else:
+                    print("regresar al menu")
+                    break
+            if compra=="3":
+                print("Se agrego al carrito")
+                broken=input("Quieres seleccionar algo mas 1(Si)-2(No)")
+                if broken=="1":
+                    break
+               
+                else:
+                    print("regresar al menu")
+                    break
+    
+    
+    
+    
+    
+    
+
+
+     
+
+
+
+
--- persistence/db.py
+++ persistence/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-CONNECTION = 'mysql+pymysql://root:dmc011505@localhost:3307/pinata'
+CONNECTION = 'mysql+pymysql://root:dmc011505@localhost:3307/nicoshop'
 
 SessionLocal = sessionmaker(bind =create_engine(CONNECTION, echo = True))
 
