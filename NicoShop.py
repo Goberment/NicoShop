0f78713eafa1296c036152cff32a595ff1111f17
negx02 <nicolasbecerramuro@gmail.com>
2025-05-21 02:23:49 -0700

    Se agrego el while para que regrese al iniciar secion o registrarse

--- NicoShop.py
+++ NicoShop.py
@@ -10,28 +10,26 @@ def get():
         print(f"{p.id}, Nombre: {p.nombre}, Precio: ${p.precio}")
 
 def save(nombre, email, password):
-    c=Cliente   (nombre=nombre, email=email, password=password)  
+    c = Cliente(nombre=nombre, email=email, password=password)  
     session.add(c)
     session.commit()
 
-
 print("-----Bienvenido a NicoShop------")
 
-Inicio=input("Iniciar sesión(1)---------Registrarse(2)---------: ")
+while True:  # aqui ase agrega bucle para que regrese al menú después del registro
+    Inicio = input("Iniciar sesion(1)---------Registrarse(2)---------: ")
 
-if Inicio=="1":
-    correo=input("Ingresa tu correo electronico: ")
-    password=input("Ingresa tu contraseña: ")
+    if Inicio == "1":
+        correo = input("Ingresa tu correo electronico: ")
+        password = input("Ingresa tu contraseña: ")
+        break  # Este break sale del bucle si inicia sesión
 
+    elif Inicio == "2":
+        nombre = input("Ingresa su nombre: ")
+        email = input("Ingresa su correo electronico: ")
+        password = input("Ingresa una contraseña: ")
+        save(nombre, email, password)
+        print("Cuenta creada con éxito. Vuelve a iniciar sesión.")  # mensaje de confirmación de que la nueva cuenta fue creada con exito
 
-
-elif(Inicio=="2"):
-    nombre=input("Ingresa su nombre: ")
-    email=input("Ingresa su correo electronico: ")
-    password=input("Ingresa una contraseña: ")
-    save(nombre, email, password)
-
-else:
-    print("Crea una cuenta")
-
-    
+    else:
+        print("Crea una cuenta")
