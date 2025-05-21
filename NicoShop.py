6e5a43399f7920fa53e1e2d3dc8b31dee99bfd3d
Goberment <mdiego@gmail.com>
2025-05-21 01:58:57 -0700

    Se agrego el registro de sesión

--- NicoShop.py
+++ NicoShop.py
@@ -9,19 +9,15 @@ def get():
     for p in producto:
         print(f"{p.id}, Nombre: {p.nombre}, Precio: ${p.precio}")
 
-def guardar(self):
-    try:
-        session.add(self)
-        session.commit()
-        print("se guardo correctamente")
-    except Exception as e:
-        session.rollback()
-        print("Error al guardar los datos")
+def save(nombre, email, password):
+    c=Cliente   (nombre=nombre, email=email, password=password)  
+    session.add(c)
+    session.commit()
 
 
 print("-----Bienvenido a NicoShop------")
 
-Inicio=input("Iniciar sesión(1)---------Registrarse(2)---------")
+Inicio=input("Iniciar sesión(1)---------Registrarse(2)---------: ")
 
 if Inicio=="1":
     correo=input("Ingresa tu correo electronico: ")
@@ -30,8 +26,12 @@ if Inicio=="1":
 
 
 elif(Inicio=="2"):
-    name=input("Ingresa su nombre: ")
-    correo=input("Ingresa su correo electronico: ")
+    nombre=input("Ingresa su nombre: ")
+    email=input("Ingresa su correo electronico: ")
     password=input("Ingresa una contraseña: ")
+    save(nombre, email, password)
+
+else:
+    print("Crea una cuenta")
 
     
